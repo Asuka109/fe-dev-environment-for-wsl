f079d5675412c0dd0bc4a9b13415010bd1d01698
Asuka109 <1097198892@qq.com>
2022-07-05 00:08:28 +0800

    chore: rename build.sh to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,3 @@
 wget https://wsldownload.azureedge.net/Ubuntu_2004.2020.424.0_x64.appx -O image.appx
 unzip -j image.appx install.tar.gz
 docker import ./install.tar.gz wsl/ubuntu:focal
-docker build -t asuka109/fe .
